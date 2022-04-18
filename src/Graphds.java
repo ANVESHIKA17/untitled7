cb04d39ec9484622bb8c9cf543d57968193e0ebe
ANVESHIKA <anveshika1717@gmail.com>
2022-04-18 10:47:37 +0530

    Initial commit

---
+++ src/Graphds.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Graphds {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> obj=new HashMap<> ();
+
+        obj.put("abc",50);
+        obj.put("abc",50);
+        System.out.println(obj.get("abc"));
+
+
+    }
+}
